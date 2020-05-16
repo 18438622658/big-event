3f7506c71c3b44084d4e13a280598d8740d8c5e3
dong <1322550220@qq.com>
2020-05-16 11:00:03 +0800

    'common完成'

---
+++ assets/js/common.js
@@ -0,0 +1,36 @@
+//放在外边加载快 没有用jq  只是js  放在入口函数里的话在页面加载晚场之后再去判断会闪一下
+//判断token是否存在  如果不存在就跳转到login.html里去
+if (!localStorage.getItem('token')) {
+    location.href = '/login.html';
+}
+
+$(function () {
+    $.ajaxPrefilter(function (options) {
+        // options就是ajax请求的里的所有配置项  包括自己发请求的时候写的和默认的属性
+        console.log(options);
+
+        // 同统一配置url   
+        options.url = 'http://www.liulongbin.top:3007' + options.url;
+        options.headers = {
+            Authorization: localStorage.getItem('token'),
+        };
+        // 发送请求之前
+        options.beforeSend = function () {
+            // 进度条加载开始
+            NProgress.start();
+        };
+
+        // 请求完成后 
+        options.complete = function (xhr) {
+            // 进度条完成消失
+            NProgress.done();
+            console.log(xhr); //xhr里有服务器返回值 在responseJSON中
+            if (xhr.responseJSON.status === 1 && xhr.responseJSON.message === '身份认证失败！') {
+                //每次ajax 请求完成之后，判断status和message 是否失败说明客户使用了一个假的token
+                localStorage.removeItem('token');
+                location.href = '/login.html'
+            }
+        };
+
+    })
+})
--- assets/js/index.js
+++ assets/js/index.js
@@ -22,32 +22,23 @@ $(function () {
 // 一定要放到函数外边  因为其他页面也要用
 function getUserInfo() {
     $.ajax({
-        type: 'get',
-        url: 'http://www.liulongbin.top:3007/my/userinfo',
+        url: '/my/userinfo',
 
         success: function (res) {
             if (res.status === 0) {
                 // 设置欢迎语   有昵称就使用昵称  没有用username
-                let name = res.data.nickname || res.data.username;
+                var name = res.data.nickname || res.data.username;
                 $('.welcome').html('欢迎你&nbsp;&nbsp' + name);
                 // 设置头像  有图片设置图片 没图片取第一个字
-                if (res.userpic) {
+                if (res.data.userpic) {
                     $('.layui-nav-img').attr('src', res.userpic).show();
                     $('.text-img').hide();
                 } else {
                     $('.layui-nav-img').hide();
-                    $('.text-img').text(name.substr(0, 1).toUpperCase());
-                    $('.text-img').css('display', 'inline-block');
+                    $('.text-img').css('display', 'inline-block').text(name.substr(0, 1).toUpperCase());
                 }
-            } else if (res.status === 1 && res.message === '身份认证失败！') {
-                localStorage.removeItem('token');
-                location.href = '/login.html'
             }
 
-        },
-        // jq中设置请求头
-        headers: {
-            Authorization: localStorage.getItem('token'),
         },
 
     })
